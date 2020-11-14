d984291989c14acc65d7b5db4f460ed4a153305c
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-14 02:41:18 -0800

    Make classes and corresponding test classes

---
+++ tests/test_board.py
@@ -0,0 +1,11 @@
+import unittest
+from minesweeper.game.board import Board
+
+
+class TestBoard(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_game.py
@@ -0,0 +1,11 @@
+import unittest
+from minesweeper.game.game import Game
+
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
