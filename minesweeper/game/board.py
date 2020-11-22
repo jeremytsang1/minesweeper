18b06e1e38a24c8613354de38e97920cfd55e939
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-22 07:55:18 -0800

    Start class Board

--- minesweeper/game/board.py
+++ minesweeper/game/board.py
@@ -1,2 +1,44 @@
+from minesweeper.game.cell import Cell
+
+
 class Board:
-    pass
+    def __init__(self, height, width):
+        if height < 0 or width < 0:
+            raise BoardNegativeDimsError(height, width)
+        self.grid = [[Cell(i, j) for j in range(width)] for i in range(height)]
+        self.height = height
+        self.width = width
+
+    def get_height(self):
+        return self.height
+
+    def get_width(self):
+        return self.width
+
+    def get_shape(self):
+        return (self.width, self.height)
+
+    def get_grid(self):
+        return self.grid
+
+    def __repr__(self):
+        WIDTH = 3
+        WALL = "|"
+        SEP = "+"
+
+    def build_row(self):
+        pass
+
+
+class BoardError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class BoardNegativeDimsError(BoardError):
+    def __init__(self, height, width):
+        super().__init__('Cannot create Board with negative dimensions '
+                         f'({height}, {width}).')
+
+if __name__ == '__main__':
+    board = Board(5, 5)
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,10 +1,55 @@
 import unittest
-from minesweeper.game.board import Board
+from minesweeper.game.board import (
+    Board,
+    BoardNegativeDimsError,
+)
+from minesweeper.game.cell import (
+    Cell
+)
 
 
 class TestBoard(unittest.TestCase):
     def setUp(self):
-        self.board = None
+        self.board = Board(10, 10)
+
+    def check_cell_predicate(self, predicate):
+        grid = self.board.get_grid()
+        for row in grid:
+            for cell in row:
+                self.assertTrue(predicate(cell))
+    # -------------------------------------------------------------------------
+
+    def test_negative_row(self):
+        with self.assertRaises(BoardNegativeDimsError):
+            self.board = Board(-5, 24)
+
+    def test_negative_col(self):
+        with self.assertRaises(BoardNegativeDimsError):
+            self.board = Board(35, -5)
+
+    def test_negative_row_and_col(self):
+        with self.assertRaises(BoardNegativeDimsError):
+            self.board = Board(-35, -5)
+
+    def test_initial_shape(self):
+        self.board = Board(39, 23)
+        grid = self.board.get_grid()
+        self.assertEqual(len(grid), 39)
+        self.assertEqual(len(grid[0]), 23)
+        self.assertEqual(self.board.get_height(), 39)
+        self.assertEqual(self.board.get_width(), 23)
+
+    def test_initial_grid_types(self):
+        height, width = 22, 49
+        self.board = Board(height, width)
+        self.check_cell_predicate(lambda cell: type(cell) == Cell)
+
+    def test_initial_cell_appearance(self):
+        height, width = 35, 67
+        self.board = Board(height, width)
+        self.check_cell_predicate(
+            lambda cell: cell.get_appearance() == Cell.Appearance.UNOPENED
+        )
 
 
 if __name__ == '__main__':
