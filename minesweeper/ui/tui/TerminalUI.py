e8d22bed719c5837fc3365ebcc1d38fc26493eeb
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-22 08:34:22 -0800

    Start class TerminalUI

--- minesweeper/ui/tui/TerminalUI.py
+++ minesweeper/ui/tui/TerminalUI.py
@@ -2,6 +2,33 @@ class TerminalUI():
     """User interface for command line version of the game.
 
     """
+    BASE = 10
+    SEP = "+"
+    LINE = "-"
+    WALL = "|"
+
     def __init__(self, board):
-        self.height = board.get_height()
-        self.width = board.get_width()
+        """Creates a new Terminal UI.
+
+        Parameters
+        ----------
+        board: Board
+            Minesweeper game board.
+
+        """
+        self.board = board
+        self.HEIGHT = board.get_height()
+        self.WIDTH = board.get_width()
+        self.CELL_WIDTH = max(self.digit_count(self.HEIGHT),
+                              self.digit_count(self.WIDTH))
+
+    @staticmethod
+    def digit_count(num):
+        tmp = -num if num < 0 else num
+        digit_count = 1
+        tmp /= TerminalUI.BASE
+
+        while tmp != 0:
+            tmp //= TerminalUI.BASE
+            digit_count += 1
+        return digit_count
---
+++ tests/test_terminal_ui.py
@@ -0,0 +1,24 @@
+import unittest
+from minesweeper.ui.tui.TerminalUI import TerminalUI
+from minesweeper.game.board import Board
+
+
+class TestTerminalUI(unittest.TestCase):
+    def setUp(self):
+        self.board = Board(10, 10)
+        self.tui = TerminalUI
+        pass
+
+    def test_digit_count(self):
+        self.assertEqual(TerminalUI.digit_count(124), 3)
+        self.assertEqual(TerminalUI.digit_count(3357395), 7)
+
+    def test_col_nums(self, ):
+        expected = (
+            "|---+---+---+---+---+---+---+---+---+---+---|\n"
+            "|   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |\n"
+            "|---+---+---+---+---+---+---+---+---+---+---|"
+        )
+
+if __name__ == '__main__':
+    unittest.main()
