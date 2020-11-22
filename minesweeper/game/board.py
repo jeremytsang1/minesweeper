941e647e91044602db9ec7a161c2d417de84fdda
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-22 08:16:22 -0800

    Test repr for class Board

--- minesweeper/game/board.py
+++ minesweeper/game/board.py
@@ -2,8 +2,11 @@ from minesweeper.game.cell import Cell
 
 
 class Board:
+    MIN_WIDTH = 1
+    MIN_HEIGHT = 1
+
     def __init__(self, height, width):
-        if height < 0 or width < 0:
+        if height < Board.MIN_HEIGHT or width < Board.MIN_WIDTH:
             raise BoardNegativeDimsError(height, width)
         self.grid = [[Cell(i, j) for j in range(width)] for i in range(height)]
         self.height = height
@@ -22,9 +25,7 @@ class Board:
         return self.grid
 
     def __repr__(self):
-        WIDTH = 3
-        WALL = "|"
-        SEP = "+"
+        return '\n'.join(' '.join(repr(cell) for cell in row) for row in self.grid)
 
     def build_row(self):
         pass
