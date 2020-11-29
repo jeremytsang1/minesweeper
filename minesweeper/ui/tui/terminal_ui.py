a4c1d2b41ba4e3d090b3c57c3a334403786e90d2
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-29 04:16:47 -0800

    Make methods to repeatedly ask for int if fail

--- minesweeper/ui/tui/terminal_ui.py
+++ minesweeper/ui/tui/terminal_ui.py
@@ -39,6 +39,31 @@ class TerminalUI():
 
     def start_game(self):
         pass
+
+    @staticmethod
+    def read_int(msg, min_val=0, max_val=MAX_INT):
+        try:
+            return TerminalUI.validate_range(int(input(f"\n{msg}\n> ")), min_val, max_val)
+        except ValueError:
+            print("Please enter an integer!")
+            return None
+
+    @staticmethod
+    def validate_range(val, min_val, max_val):
+        if val in range(min_val, max_val):
+            return val
+        else:
+            print(
+                "Please enter an int in the interval",
+                TerminalUI.make_range_string(min_val, max_val)
+            )
+
+            return None
+
+    @staticmethod
+    def make_range_string(min_val, max_val):
+        return f"[{min_val} (inclusive) ... {max_val} (exclusive)]"
+
     @staticmethod
     def make_menu_str(menu):
         return "\n".join(menu)
