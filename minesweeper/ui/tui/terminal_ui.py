3b66c68e3b5f4a979f74f2f957df11b4a1a68c29
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-29 03:49:35 -0800

    Make menu constants

--- minesweeper/ui/tui/terminal_ui.py
+++ minesweeper/ui/tui/terminal_ui.py
@@ -5,6 +5,31 @@ class TerminalUI():
     """Command Line Interface for Minesweeper
 
     """
+    MAX_INT = 1000000
+    MAIN_MENU = (
+        "\n1. New Game"
+        "\n2. Quit"
+    )
+    NEW_GAME_MENU = (
+        "\n1. Easy"
+        "\n2. Medium"
+        "\n3. Hard"
+        "\n4. Custom"
+    )
+    HEIGHT_PROMPT = "\nHow many rows?"
+    WIDTH_PROMPT = "\nHow many cols?"
+    BOMB_PROMPT = (
+        "\nHow many bombs? (must be in the range "
+        "[1 (inclusive) ... {} (exclusive)])"
+    )
+    GAME_MENU = (
+        "\n1. Open single cell"
+        "\n2. Open all adjacents to a particular cell"
+        "\n3. Quit."
+    )
+    ROW_PROMPT = "\nWhich row?"
+    COL_PROMPT = "\nWhich col?"
+
     def __init__(self, ):
         self.game = None
 
