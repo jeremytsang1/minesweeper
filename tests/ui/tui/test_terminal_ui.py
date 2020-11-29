a2fb88d0058312d369daf35faaf186ab18135a82
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-29 05:09:27 -0800

    Add exmple for user input
    See https://stackoverflow.com/a/21047132
    
    Also may be interested in https://stackoverflow.com/a/24897297

--- tests/ui/tui/test_terminal_ui.py
+++ tests/ui/tui/test_terminal_ui.py
@@ -1,12 +1,30 @@
 import unittest
+from unittest.mock import patch
 from minesweeper.ui.tui.terminal_ui import TUI
 from minesweeper.ui.tui.command_line_grid import CommandLineGrid
 from minesweeper.game.board import Board
+
+MODULE_NAME = 'tests.ui.tui.test_terminal_ui'
+
+def get_input(text):
+    return input(text)
+
+def answer():
+    ans = get_input('enter yes or no')
+    if ans == 'yes':
+        return 'you entered yes'
+    if ans == 'no':
+        return 'you entered no'
 
 
 class TestTerminalUI(unittest.TestCase):
     def setUp(self):
         self.tui = TUI()
+
+    @patch(f'{MODULE_NAME}.get_input', return_value='yes')
+    def test_answer_yes(self, input):
+        self.assertEqual(answer(), 'you entered yes')
+
 
     # def test_render_board_artificial(self):
     #     height, width = 10, 10
