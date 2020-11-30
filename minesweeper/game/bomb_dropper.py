4554295ae263e0d04efbea2de5d8cc07e6852c89
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-30 02:57:25 -0800

    Add assertions

--- minesweeper/game/bomb_dropper.py
+++ minesweeper/game/bomb_dropper.py
@@ -5,6 +5,8 @@ class BombDropper():
     DEFAULT_SIZE = 10
 
     def __init__(self, height=DEFAULT_SIZE, width=DEFAULT_SIZE):
+        assert height > 0
+        assert width > 0
         self.height = height
         self.width = width
 
