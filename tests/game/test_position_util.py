87dc51785ac7b18a87e5058f7e7cc3b01ca0772b
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-30 04:56:41 -0800

    Use helper method to remove redundant code

--- tests/game/test_position_util.py
+++ tests/game/test_position_util.py
@@ -9,6 +9,11 @@ class TestPositionUtil(unittest.TestCase):
     def setUp(self):
         self.pos_util = PositionUtil(10, 10)
 
+    def cmp(self, shape, pos, expected):
+        self.pos_util = PositionUtil(*shape)
+        actual = self.pos_util.adj(*pos)
+        self.assertEqual(actual, expected)
+
     def test_negative_components(self):
         with self.assertRaises(InvalidComponent):
             self.pos_util = PositionUtil(-10, -2)
@@ -18,47 +23,40 @@ class TestPositionUtil(unittest.TestCase):
             self.pos_util = PositionUtil(0, 2)
 
     def test_adjacent_one_row_left(self):
-        self.pos_util = PositionUtil(1, 5)
+        shape = (1, 5)
         pos = (0, 0)
         expected = {(0, 1)}
-        actual = self.pos_util.adj(*pos)
-        self.assertEqual(actual, expected)
+        self.cmp(shape, pos, expected)
 
     def test_adjacent_one_row_right(self):
-        self.pos_util = PositionUtil(1, 5)
+        shape = (1, 5)
         pos = (0, 4)
         expected = {(0, 3)}
-        actual = self.pos_util.adj(*pos)
-        self.assertEqual(actual, expected)
+        self.cmp(shape, pos, expected)
 
     def test_adjacent_one_row_interior(self):
-        self.pos_util = PositionUtil(1, 5)
+        shape = (1, 5)
         pos = (0, 1)
         expected = {(0, 0), (0, 2)}
-        actual = self.pos_util.adj(*pos)
-        self.assertEqual(actual, expected)
+        self.cmp(shape, pos, expected)
 
     def test_adjacent_one_col_top(self):
-        self.pos_util = PositionUtil(5, 1)
+        shape = (5, 1)
         pos = (0, 0)
         expected = {(1, 0)}
-        actual = self.pos_util.adj(*pos)
-        self.assertEqual(actual, expected)
+        self.cmp(shape, pos, expected)
 
     def test_adjacent_one_col_bottom(self):
-        self.pos_util = PositionUtil(5, 1)
+        shape = (5, 1)
         pos = (4, 0)
         expected = {(3, 0)}
-        actual = self.pos_util.adj(*pos)
-        self.assertEqual(actual, expected)
+        self.cmp(shape, pos, expected)
 
     def test_adjacent_one_col_interior(self):
-        self.pos_util = PositionUtil(5, 1)
+        shape = (5, 1)
         pos = (1, 0)
         expected = {(0, 0), (2, 0)}
-        actual = self.pos_util.adj(*pos)
-        self.assertEqual(actual, expected)
-
+        self.cmp(shape, pos, expected)
 
 if __name__ == '__main__':
     unittest.main()
