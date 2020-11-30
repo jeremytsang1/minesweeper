0c10e60192aab9451975a7456207819ca48573ed
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-30 03:33:00 -0800

    Add more parameters and validators

--- minesweeper/game/bomb_dropper.py
+++ minesweeper/game/bomb_dropper.py
@@ -3,12 +3,30 @@ class BombDropper():
 
     """
     DEFAULT_SIZE = 10
+    DEFAULT_CLICK_ROW = 0
+    DEFAULT_CLICK_COL = 0
+    DEFAULT_BOMB_COUNT = 1
 
-    def __init__(self, height=DEFAULT_SIZE, width=DEFAULT_SIZE):
-        BombDropper.validate_components(height, width)
+    def __init__(self,
+                 height=DEFAULT_SIZE,
+                 width=DEFAULT_SIZE,
+                 initial_click_row=DEFAULT_CLICK_ROW,
+                 initial_click_col=DEFAULT_CLICK_COL,
+                 bomb_count=DEFAULT_BOMB_COUNT):
+        BombDropper.validate_input(height, width, initial_click_row,
+                                   initial_click_col, bomb_count)
         self.height = height
         self.width = width
         self.bombs = self.drop_bombs()
+
+    # -------------------------------------------------------------------------
+    # Validation methods
+
+    @staticmethod
+    def validate_input(height, width, row, col, bomb_count):
+        BombDropper.validate_components(height, width)
+        BombDropper.validate_bomb_count(height, width, bomb_count)
+        BombDropper.validate_initial_click(height, width, row, col)
 
     @staticmethod
     def validate_components(height, width):
@@ -16,6 +34,19 @@ class BombDropper():
             raise InvalidComponent(height, "height")
         if width <= 0:
             raise InvalidComponent(width, "width")
+
+    @staticmethod
+    def validate_bomb_count(height, width, bomb_count):
+        if bomb_count <= 0 or height * width <= bomb_count:
+            raise InvalidBombCount(height, width, bomb_count)
+
+    @staticmethod
+    def validate_initial_click(height, width, row, col):
+        if row not in range(height) or col not in range(width):
+            raise InvalidInitialClick(height, width, row, col)
+
+    # -------------------------------------------------------------------------
+    # getters
 
     def get_height(self):
         return self.height
@@ -25,6 +56,9 @@ class BombDropper():
 
     def get_bombs(self):
         return self.bombs
+
+    # -------------------------------------------------------------------------
+    # Bomb laying
 
     def initialize_empty_bomb_field(self):
         return [[False for _ in range(self.width)] for _ in range(self.height)]
@@ -43,4 +77,19 @@ class InvalidComponent(BombDropperError):
     def __init__(self, component, name):
         super().__init__(
             f'BombDropper\'s {name} is non-positive and has value {component}.'
+        )
+
+
+class InvalidBombCount(BombDropperError):
+    def __init__(self, height, width, bomb_count):
+        super().__init__(
+            f'Invalid number of bombs ({bomb_count}) for {height} x {width}'
+            + ' grid'
+        )
+
+
+class InvalidInitialClick(BombDropperError):
+    def __init__(self, height, width, row, col):
+        super().__init__(
+            f'Initial click ({row}, {col})is outside {height} x {width} grid'
         )
