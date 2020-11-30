31f7c1065cd65203652d6623e8f77452b888af34
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-30 04:49:47 -0800

    Make single row tests for PositionUtil

--- tests/game/test_position_util.py
+++ tests/game/test_position_util.py
@@ -17,6 +17,27 @@ class TestPositionUtil(unittest.TestCase):
         with self.assertRaises(InvalidComponent):
             self.pos_util = PositionUtil(0, 2)
 
+    def test_adjacent_one_row_left(self):
+        self.pos_util = PositionUtil(1, 5)
+        pos = (0, 0)
+        expected = {(0, 1)}
+        actual = self.pos_util.adj(*pos)
+        self.assertEqual(actual, expected)
+
+    def test_adjacent_one_row_right(self):
+        self.pos_util = PositionUtil(1, 5)
+        pos = (0, 4)
+        expected = {(0, 3)}
+        actual = self.pos_util.adj(*pos)
+        self.assertEqual(actual, expected)
+
+    def test_adjacent_one_row_interior(self):
+        self.pos_util = PositionUtil(1, 5)
+        pos = (0, 1)
+        expected = {(0, 0), (0, 2)}
+        actual = self.pos_util.adj(*pos)
+        self.assertEqual(actual, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
