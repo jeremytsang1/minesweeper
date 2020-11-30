f8ca64fa47171facf63535b4de5e71b9d41f9218
Jeremy Tsang <jeremytsang1@gmail.com>
2020-11-30 03:02:52 -0800

    Make method to initialize 2D list of all False

--- minesweeper/game/bomb_dropper.py
+++ minesweeper/game/bomb_dropper.py
@@ -9,9 +9,13 @@ class BombDropper():
         assert width > 0
         self.height = height
         self.width = width
+        self.bombs = self.initialize_empty_bomb_field()
 
     def get_height(self):
         return self.height
 
     def get_width(self):
         return self.width
+
+    def initialize_empty_bomb_field(self):
+        return [[False for _ in range(self.width)] for _ in range(self.height)]
--- tests/game/test_bomb_layer.py
+++ tests/game/test_bomb_layer.py
@@ -15,7 +15,39 @@ class TestBombDropper(unittest.TestCase):
         self.assertEqual(self.bd.get_height(), 35)
         self.assertEqual(self.bd.get_width(), 29)
 
+    def test_initialize_empty_bomb_field(self):
+        self.bd = BombDropper(2, 5)
+        expected = [
+            [False, False, False, False, False],
+            [False, False, False, False, False],
+        ]
+        actual = self.bd.initialize_empty_bomb_field()
+        self.assertEqual(actual, expected)
 
+    def test_initialize_empty_bomb_field_1_row(self):
+        self.bd = BombDropper(1, 10)
+        expected = [
+            [False, False, False, False, False, False, False, False, False, False]
+        ]
+        actual = self.bd.initialize_empty_bomb_field()
+        self.assertEqual(actual, expected)
+
+    def test_initialize_empty_bomb_field_1_col(self):
+        self.bd = BombDropper(10, 1)
+        expected = [
+            [False],
+            [False],
+            [False],
+            [False],
+            [False],
+            [False],
+            [False],
+            [False],
+            [False],
+            [False],
+        ]
+        actual = self.bd.initialize_empty_bomb_field()
+        self.assertEqual(actual, expected)
 
 if __name__ == '__main__':
     unittest.main()
