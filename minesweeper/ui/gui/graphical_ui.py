b85abc3a8d5ab0a73eeac98b63603387c7ea961c
Jeremy Tsang <jeremytsang1@gmail.com>
2020-12-05 16:53:34 -0800

    Compute dimensions and display game screen

--- minesweeper/ui/gui/graphical_ui.py
+++ minesweeper/ui/gui/graphical_ui.py
@@ -1,6 +1,7 @@
 import pygame
 from minesweeper.ui.gui.main_menu import MainMenu
 from minesweeper.ui.gui.difficulty import Difficulty
+from minesweeper.ui.gui.gui_board import GUIBoard
 from minesweeper.game.game import Game
 
 
@@ -23,7 +24,19 @@ class GUI():
         self.main_menu.show_menu(self.screen)
 
     def run_game(self):
-        raise NotImplementedError
+        self.game = None
+        dimensions = GUIBoard.compute_dimensions(*self.difficulty.get_shape())
+        self.screen = pygame.display.set_mode(dimensions)
+        self.gui_board = GUIBoard(
+            *self.difficulty.get_shape(),
+            offset_x=0,
+            offset_y=0,
+        )
+        running = True
+
+        while running:
+            self.screen.fill((255, 255, 255))
+            pygame.display.flip()
 
 
 if __name__ == '__main__':
