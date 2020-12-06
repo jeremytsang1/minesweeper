55bc591bada5b71bcffbe37788129def6a54d327
Jeremy Tsang <jeremytsang1@gmail.com>
2020-12-05 21:28:18 -0800

    Attach appropriate fill to each image

--- minesweeper/ui/gui/gui_cell.py
+++ minesweeper/ui/gui/gui_cell.py
@@ -18,21 +18,21 @@ class GUICell(pygame.sprite.Sprite):
 
     # Can't use comprehensions in class level variables:
     # https://stackoverflow.com/a/28130950
-    FILENAMES = {
-        1: os.path.join(DIR_NUMBERS, '1.png'),
-        2: os.path.join(DIR_NUMBERS, '2.png'),
-        3: os.path.join(DIR_NUMBERS, '3.png'),
-        4: os.path.join(DIR_NUMBERS, '4.png'),
-        5: os.path.join(DIR_NUMBERS, '5.png'),
-        6: os.path.join(DIR_NUMBERS, '6.png'),
-        7: os.path.join(DIR_NUMBERS, '7.png'),
-        8: os.path.join(DIR_NUMBERS, '8.png'),
+    IMAGES = {
+        1: {'filename': os.path.join(DIR_NUMBERS, '1.png'), 'fill': '#000000'},
+        2: {'filename': os.path.join(DIR_NUMBERS, '2.png'), 'fill': '#000000'},
+        3: {'filename': os.path.join(DIR_NUMBERS, '3.png'), 'fill': '#000000'},
+        4: {'filename': os.path.join(DIR_NUMBERS, '4.png'), 'fill': '#000000'},
+        5: {'filename': os.path.join(DIR_NUMBERS, '5.png'), 'fill': '#000000'},
+        6: {'filename': os.path.join(DIR_NUMBERS, '6.png'), 'fill': '#000000'},
+        7: {'filename': os.path.join(DIR_NUMBERS, '7.png'), 'fill': '#000000'},
+        8: {'filename': os.path.join(DIR_NUMBERS, '8.png'), 'fill': '#000000'},
         # 'square_black': os.path.join(DIR_SQUARES, 'square_black.png'),
-        Cell.Appearance.FLAG: os.path.join(DIR_FLAGS, 'flag.png'),
-        Cell.Appearance.UNOPENED: os.path.join(DIR_SQUARES, 'square_blue.png'),
-        Cell.Appearance.EMPTY: os.path.join(DIR_SQUARES, 'square_empty.png'),
-        Cell.Appearance.UNENCOUNTERED_BOMB: os.path.join(DIR_BOMBS, 'bomb_black.png'),
-        Cell.Appearance.OPENED_BOMB: os.path.join(DIR_BOMBS, 'bomb_red.png'),
+        Cell.Appearance.FLAG: {'filename': os.path.join(DIR_FLAGS, 'flag.png'), 'fill': '#FFFFFF'},
+        Cell.Appearance.UNOPENED: {'filename': os.path.join(DIR_SQUARES, 'square_blue.png'), 'fill': '#000000'},
+        Cell.Appearance.EMPTY: {'filename': os.path.join(DIR_SQUARES, 'square_empty.png'), 'fill': '#000000'},
+        Cell.Appearance.UNENCOUNTERED_BOMB: {'filename': os.path.join(DIR_BOMBS, 'bomb_black.png'), 'fill': '#000000'},
+        Cell.Appearance.OPENED_BOMB: {'filename': os.path.join(DIR_BOMBS, 'bomb_red.png'), 'fill': '#000000'},
     }
 
     def __init__(self, x, y):
@@ -40,8 +40,8 @@ class GUICell(pygame.sprite.Sprite):
         self.load_image(Cell.Appearance.UNOPENED)
 
     def load_image(self, appearance):
-        filename = self.FILENAMES[appearance]
+        filename = self.IMAGES[appearance]['filename']
+        fill = self.IMAGES[appearance]['fill']
         self.surf = pygame.image.load(filename).convert()
-        self.surf.set_colorkey((0, 0, 0), RLEACCEL)
-
+        self.surf.set_colorkey(fill, RLEACCEL)
         self.rect = self.surf.get_rect()
