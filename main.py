00c590b1b8eff5426a18cea9bdb0f7d7f0ce341b
Jeremy Tsang <jeremytsang1@gmail.com>
2020-12-06 21:01:44 -0800

    Make script to choose whether to start TUI or GUI

--- main.py
+++ main.py
@@ -5,11 +5,56 @@
 #               version.
 
 from minesweeper.ui.tui.terminal_ui import TUI
+from minesweeper.ui.gui.graphical_ui import GUI
 
 
 def main():
-    ui = TUI()
-    ui.run_main_menu()
+    play_mode_menu()
+
+
+def play_mode_menu():
+    menu_options = {
+        1: run_tui,
+        2: run_gui,
+        3: quit_game,
+    }
+    usr_input = None
+
+    while usr_input not in menu_options:
+        usr_input = input(get_menu_options())
+        try:
+            usr_input = int(usr_input)
+            if usr_input not in menu_options:
+                print("\nPlease choose an appropriate menu option!")
+        except ValueError:
+            print("\nPlease enter an integer!")
+
+    menu_options[usr_input]()
+
+
+def get_menu_options():
+    return (
+        "\n1. Play in TUI (text mode) "
+        "\n2. Play in GUI (graphical mode)"
+        "\n3. Quit"
+        "\n> "
+    )
+
+
+def run_tui():
+    print("\nStarting in TUI mode.")
+    tui = TUI()
+    tui.run_main_menu()
+
+
+def run_gui():
+    print("\nStarting in GUI mode.")
+    gui = GUI()
+    gui.run_main_menu()
+
+
+def quit_game():
+    print("\nBye! Have a nice day!")
 
 
 if __name__ == '__main__':
