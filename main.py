63ae3a53716d0096362cf08e3b708bc1ec54c7fc
Jeremy Tsang <jeremytsang1@gmail.com>
2020-12-06 23:19:39 -0800

    Make general menu function for main.py

--- main.py
+++ main.py
@@ -12,33 +12,68 @@ def main():
     play_mode_menu()
 
 
-def play_mode_menu():
-    menu_options = {
-        1: run_tui,
-        2: run_gui,
-        3: quit_game,
-    }
-    usr_input = None
+def get_input(prompts):
+    """Given a list of prompts asks the user to input one of the them (by index).
 
-    while usr_input not in menu_options:
-        usr_input = input(get_menu_options())
+    Parameters
+    ----------
+    prompts: iterable of str
+        Descriptions of menu options to display to user.
+
+    Returns
+    -------
+    int
+        The user's choice.
+    """
+    usr_input = None
+    option_numbers = range(len(prompts))
+    menu_str = format_prompts(prompts)
+
+    while usr_input not in option_numbers:
+        usr_input = input(menu_str)
         try:
             usr_input = int(usr_input)
-            if usr_input not in menu_options:
+            if usr_input not in option_numbers:
                 print("\nPlease choose an appropriate menu option!")
         except ValueError:
             print("\nPlease enter an integer!")
 
-    menu_options[usr_input]()
+    return usr_input
 
 
-def get_menu_options():
-    return (
-        "\n1. Play in TUI (text mode) "
-        "\n2. Play in GUI (graphical mode)"
-        "\n3. Quit"
-        "\n> "
+def format_prompts(descriptions):
+    """Concatenate menu options into a single multiline string.
+
+    Parameters
+    ----------
+    descriptions: iterable of str
+        Descriptions for individual menu options.
+
+    Returns
+    -------
+    str
+        One description per line with an extra line for the prompt for the user
+        to enter their choice.
+
+    """
+    return '\n'.join(
+        [f'{i}. {description}' for i, description in enumerate(descriptions)]
+        + ["> "])
+
+
+def play_mode_menu():
+    ACTION = 'action'
+    DESCRIPTION = 'description'
+    menu_options = (
+        {ACTION: run_tui,
+         DESCRIPTION: "Play in TUI (text mode) "},
+        {ACTION: run_gui,
+         DESCRIPTION: "Play in GUI (graphical mode)"},
+        {ACTION: quit_game,
+         DESCRIPTION: "Quit"},
     )
+    usr_input = get_input([option[DESCRIPTION] for option in menu_options])
+    menu_options[usr_input][ACTION]()
 
 
 def run_tui():
