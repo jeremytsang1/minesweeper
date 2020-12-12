74c436f33a60be5535681a430a2a7cd064861659
Jeremy Tsang <jeremytsang1@gmail.com>
2020-12-12 02:00:31 -0800

    Add validation for Menu.__init__()

--- minesweeper/ui/tui/menu.py
+++ minesweeper/ui/tui/menu.py
@@ -17,8 +17,17 @@ class Menu():
             Lines to display to the user for each option.
 
         """
+        self._validate_option_descriptions(option_descriptions)
         self.option_descriptions = option_descriptions
         self.count = len(option_descriptions)
+
+    @staticmethod
+    def _validate_option_descriptions(option_descriptions):
+        for description in option_descriptions:
+            if type(description) != str:
+                raise ValueError(
+                    "`option_descriptions` has a non-string element"
+                )
 
     def _format_prompts(self, offset=1):
         """Concatenate menu options into a single multiline string.
