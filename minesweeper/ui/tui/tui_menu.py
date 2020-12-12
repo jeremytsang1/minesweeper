ee93301b0c6252e28af0468672c4ea5e68c50c9c
Jeremy Tsang <jeremytsang1@gmail.com>
2020-12-11 20:18:56 -0800

    Add terminal_ui menu code to its own class

---
+++ minesweeper/ui/tui/tui_menu.py
@@ -0,0 +1,41 @@
+class TUIMenu():
+    """Menu for Terminal User Interface.
+
+    """
+    PROMPT_APPEARANCE = "\n{}\n> "
+
+    def __init__(self, prompts, offset=1):
+        self.prompts = prompts
+        self.offset = offset
+
+    @staticmethod
+    def read_menu_option(menu):
+        return TUIMenu.read_int(TUIMenu.make_menu_str(menu), 1, len(menu) + 1)
+
+    @staticmethod
+    def read_int(self, msg, min_val, max_val):
+        usr_input = None
+        while usr_input is None:
+            try:
+                usr_input = int(input(TUIMenu.PROMPT_APPEARANCE.format(msg)))
+                usr_input = TUIMenu.validate_range(usr_input, min_val, max_val)
+            except ValueError:
+                print("\nPlease enter an integer!")
+                usr_input = None
+        return usr_input
+
+    @staticmethod
+    def validate_range(val, min_val, max_val):
+        if val in range(min_val, max_val):
+            return val
+        else:
+            print(
+                "\nPlease enter an int in the interval",
+                TUIMenu.make_range_string(min_val, max_val)
+            )
+
+            return None
+
+    @staticmethod
+    def make_range_string(min_val, max_val):
+        return f"[{min_val} (inclusive) ... {max_val} (exclusive)]"
