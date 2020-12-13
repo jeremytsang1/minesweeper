b44f44016f79c0a975c050a270b2d4ee1be9fa45
Jeremy Tsang <jeremytsang1@gmail.com>
2020-12-13 00:35:28 -0800

    Add docstrings and replace function with constant

--- minesweeper/ui/tui/io.py
+++ minesweeper/ui/tui/io.py
@@ -1,6 +1,32 @@
 class IO():
+    OUT_OF_RANGE_MESSAGE = (
+        "\nPlease enter an int in the interval"
+        "[{} (inclusive) ... {} (exclusive)]"
+    )
+
     @staticmethod
     def read_int(msg, min_val, max_val):
+        """Display a prompt message to the user and get input within a given
+        range.
+
+        Keeps prompting user for input until user submits a valid integer in
+        range.
+
+        Parameters
+        ----------
+        msg: str
+            Message to display to user.
+        min_val: int
+            Smallest value (inclusive) to allow user to enter.
+        max_val: int
+            Largest value (exclusive)_ to allow user to enter.
+
+        Returns
+        -------
+        int
+            User input as an int.
+
+        """
         usr_input = None
         while usr_input is None:
             try:
@@ -13,16 +39,26 @@ class IO():
 
     @staticmethod
     def validate_range(val, min_val, max_val):
+        """Checks if a value is in a given range.
+
+        In addition, prints a message if the value is not within range.
+
+        Parameters
+        ----------
+        val: int
+            Value to check.
+        min_val: int
+            Lower bound (inclusive) of the allowed range.
+        max_val: int
+            Upper bound (exclusive) of the allowed range.
+
+        Returns
+        -------
+        int or None
+            Returns the value if it is within range otherwise returns None.
+        """
         if val in range(min_val, max_val):
             return val
         else:
-            print(
-                "\nPlease enter an int in the interval",
-                IO.make_range_string(min_val, max_val)
-            )
-
+            print(IO.OUT_OF_RANGE_MESSAGE.format(min_val, max_val))
             return None
-
-    @staticmethod
-    def make_range_string(min_val, max_val):
-        return f"[{min_val} (inclusive) ... {max_val} (exclusive)]"
